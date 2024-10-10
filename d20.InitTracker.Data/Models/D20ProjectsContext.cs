d22c1ee6964a07a69faa87c88508c8d25754ccfd
Jeff Paulson <3598349+Jrpaulson03@users.noreply.github.com>
2024-10-09 22:55:28 -0700

    Update D20ProjectsContext.cs

--- d20.InitTracker.Data/Models/D20ProjectsContext.cs
+++ d20.InitTracker.Data/Models/D20ProjectsContext.cs
@@ -25,7 +25,7 @@ public partial class D20ProjectsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("data source=tcp:jpaulson.database.windows.net,1433;initial catalog=d20Projects;user id=jpaulson@outlook.com@jpaulson;password=Jester123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("data source=tcp:jpaulson.database.windows.net,1433;initial catalog=d20Projects;user id=jpaulson@outlook.com@jpaulson;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
